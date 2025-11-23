3e63ee1212bd30b12b3f7e5f2935f32f5dbf76fa
Seba-SL <slazo@fi.uba.ar>
2025-11-23 19:53:01 -0300

    la otra curva tmb la de psi_s vs VG

--- teoricos.py
+++ teoricos.py
@@ -4,6 +4,17 @@ import matplotlib.pyplot as plt
 # Parámetros
 e_o = 88.54e-15  # F/cm
 epsilon_si = 11.7 * e_o
+e_ox = 3.9*e_o
+
+
+
+chi_si = 4.05  #eV
+chi_ox  =0.95 #eV
+
+Eg = 1.12 #eV
+
+
+tox = 16e-9
 
 q = 1.602e-19
 k = 1.381e-23
@@ -14,7 +25,15 @@ V_th = (k*T)/q
 ni = 1e10   # cm^-3
 Na = 2e16   # cm^-3
 
+Cox = e_ox/tox
+
 psi_B = V_th * np.log(Na / ni)  # Para sustrato tipo p
+
+psi_bi = chi_si*q + (Eg/2*q) + V_th*np.log(Na/ni) - chi_ox*q
+
+VFB = - psi_bi 
+
+
 
 # Rango psi_s
 psi_s_values = np.linspace(-0.2, 0.8, 200000)
@@ -51,6 +70,8 @@ Q_s_inversion = np.abs(np.sqrt(2 * epsilon_si * q * Na * psi_s_values
                        * np.exp(psi_s_values/V_th)))
 Q_s_inversion[~mask_inversion] = np.nan
 
+
+
 # ---- Gráfica ----
 plt.figure(figsize=(6,4))
 plt.plot(psi_s_values, Q_s_abs, alpha=0.7, linewidth=4.5, label="Exacto")
@@ -82,4 +103,44 @@ plt.gca().text(
 )
 
 plt.tight_layout()
+plt.show()
+
+
+
+
+
+#psi_s
+
+C = np.sqrt(2*epsilon_si*q*V_th*Na)
+Qs = lambda phi_s: C * np.sqrt((np.e**(-phi_s / V_th) + (phi_s / V_th) - 1) + (1.01e10/Na) * (np.e**(phi_s / V_th) - (phi_s / V_th) - 1))
+Vg = lambda phi_s: phi_s + (Qs(phi_s) / Cox) + VFB if phi_s > 0 else phi_s - (Qs(phi_s) / Cox) + VFB 
+
+
+
+plt.figure(figsize=(6,4))
+y = np.linspace(-2, 1, 10000)
+
+
+# Cartel
+textstr = '\n'.join((
+    r'$T = %d\,\mathrm{K}$' % T,
+    r'$N_{sub} = N_{ch} = %.1e \mathrm{cm^{-3}}$' % Na,
+    r'$\psi_B = %.3f\,\mathrm{V}$' % psi_B
+))
+
+plt.gca().text(
+    0.60, 0.30,
+    textstr,
+    transform=plt.gca().transAxes,
+    fontsize=10,
+    bbox=dict(boxstyle='round', facecolor='white', alpha=0.9)
+)
+
+plt.ylabel(r'$\psi_s$ (V)')
+plt.xlabel(r'$V_G$ (V)')
+plt.title(r"Curva de $\psi_s$ vs $V_G$")
+plt.plot([Vg(_y) for _y in y], y,alpha = 0.85, linewidth=4.5, color = "orange")
+plt.xlim(-2, 1)
+# plt.ylim(0, 1.2)
+plt.grid()
 plt.show()
